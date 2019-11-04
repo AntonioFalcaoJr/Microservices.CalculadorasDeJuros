ad7780a6aba94de6c0aa6162ab1eac71cf1a311a
Antonio Falcao <arfj@edu.univali.br>
2019-11-03 21:01:04 -0300

    ShowMeTheCode Controller / CrossCutting

---
+++ 1 - Presentation/Microservices.CalculadorasDeJuros.Api/Controllers/ApiV1/ShowMeTheCodeController.cs
@@ -0,0 +1,16 @@
+﻿using Microservices.CalculadorasDeJuros.Api.Controllers.ApiV1.Base;
+using Microservices.CalculadorasDeJuros.CrossCutting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.CalculadorasDeJuros.Api.Controllers.ApiV1
+{
+    [Route("v{version:apiVersion}/showMeTheCode")]
+    public class ShowMeTheCodeController : ApiV1BaseController
+    {
+        [HttpGet]
+        public IActionResult Get() =>
+           Ok(CalculadoraDeJurosContants.MsCalculadoraDeJurosGitHubRepository + "\n" +
+              CalculadoraDeJurosContants.MsDeTaxasDeJurosGitHubRepository + "\n" +
+              CalculadoraDeJurosContants.CalculadoraDeJurosContratosGitHubRepository);
+    }
+}
---
+++ 4 - CrossCutting/Microservices.CalculadorasDeJuros.CrossCutting/CalculadoraDeJurosContants.cs
@@ -0,0 +1,9 @@
+﻿namespace Microservices.CalculadorasDeJuros.CrossCutting
+{
+    public class CalculadoraDeJurosContants
+    {
+        public const string CalculadoraDeJurosContratosGitHubRepository = "https://github.com/AntonioFalcao/CalculadoraDeJuros.Contratos";
+        public const string MsCalculadoraDeJurosGitHubRepository = "https://github.com/AntonioFalcao/Microservices.CalculadorasDeJuros";
+        public const string MsDeTaxasDeJurosGitHubRepository = "https://github.com/AntonioFalcao/Microservices.TaxaDeJuros";
+    }
+}
